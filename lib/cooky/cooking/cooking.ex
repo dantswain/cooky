a6bec5ec9570a02b3f478e5b82ac84faec5fac29
Dan Swain <dan.t.swain@gmail.com>
2017-10-11 10:22:47 -0500

    More refactoring, fix bug

--- lib/cooky/cooking/cooking.ex
+++ lib/cooky/cooking/cooking.ex
@@ -19,44 +19,40 @@ defmodule Cooking do
   end
 
   defp check_recipes(ingredient_map, recipe_pool, acc) do
-    case do_check_recipes(ingredient_map, recipe_pool) do
-      {[], _} -> {acc, ingredient_map}
-      {satisfied_recipes, updated_ingredient_map} ->
-        check_recipes(updated_ingredient_map, recipe_pool, acc ++ satisfied_recipes)
+    case first_satisfied_recipe(ingredient_map, recipe_pool) do
+      nil ->
+        {acc, ingredient_map}
+      satisfied_recipe ->
+        updated_ingredient_map = deselect_recipe(ingredient_map, satisfied_recipe)
+        check_recipes(updated_ingredient_map, recipe_pool, [satisfied_recipe | acc])
     end
   end
 
-  defp do_check_recipes(ingredient_map, recipes) do
-    satisfied_recipes = Enum.filter(
+  defp first_satisfied_recipe(ingredient_map, recipes) do
+    Enum.find(
       recipes,
       &(IngredientMap.satisfies_recipe?(ingredient_map, &1))
     )
+  end
 
-    updated_ingredient_map = Enum.reduce(
-      used_ingredients(satisfied_recipes),
+  defp deselect_recipe(ingredient_map, recipe) do
+    Enum.reduce(
+      used_ingredients(recipe),
       ingredient_map,
       fn({ingredient_id, qty}, ingredient_map_acc) ->
         IngredientMap.deselect_ingredient(ingredient_map_acc, ingredient_id, qty)
       end
     )
-
-    {satisfied_recipes, updated_ingredient_map}
   end
 
-  defp used_ingredients(recipes) do
-    recipes
-    |> Enum.map(
-      fn(recipe) ->
-        Enum.map(
-          recipe.recipe_ingredients,
-          fn(recipe_ingredient) ->
-            {
-              recipe_ingredient.ingredient_id,
-              recipe_ingredient.quantity
-            }
-          end)
-      end
-    )
-    |> List.flatten
+  defp used_ingredients(recipe) do
+    Enum.map(
+      recipe.recipe_ingredients,
+      fn(recipe_ingredient) ->
+        {
+          recipe_ingredient.ingredient_id,
+          recipe_ingredient.quantity
+        }
+      end)
   end
 end
--- test/cooking/cooking_test.exs
+++ test/cooking/cooking_test.exs
@@ -70,7 +70,7 @@ defmodule CookingTest do
       Cooking.check_recipes(ingredient_map, recipes)
   end
 
-  test "checking recipes - match multiple recipes" do
+  test "checking recipes - don't overmatch" do
     batter_type = Fixture.create_ingredient_type("batter")
     filling_type = Fixture.create_ingredient_type("filling")
 
@@ -87,17 +87,49 @@ defmodule CookingTest do
                      |> IngredientMap.select_ingredient(batter.id)
                      |> IngredientMap.select_ingredient(batter.id)
                      |> IngredientMap.select_ingredient(chips.id)
-                     |> IngredientMap.select_ingredient(batter.id)
                      |> IngredientMap.select_ingredient(chips.id)
-                     |> IngredientMap.select_ingredient(chips.id)
-                     |> IngredientMap.select_ingredient(nuts.id)
 
     recipes = [boring_recipe, chips_recipe, super_chips_recipe]
 
+    {satisfied_recipes, updated_ingredient_map} =
+      Cooking.check_recipes(ingredient_map, recipes)
+    assert 2 == length(satisfied_recipes)
+
+    Enum.each(updated_ingredient_map, fn({_id, ingredient}) ->
+      assert ingredient.selected_count >= 0
+    end)
+  end
+
+  test "checking recipes - match multiple recipes" do
+    batter_type = Fixture.create_ingredient_type("batter")
+    filling_type = Fixture.create_ingredient_type("filling")
+
+    boring_batter = Fixture.create_ingredient("boring batter", batter_type)
+    batter = Fixture.create_ingredient("batter", batter_type)
+    chips = Fixture.create_ingredient("chips of some sort", filling_type)
+    nuts = Fixture.create_ingredient("brazil nuts", filling_type)
+    hair = Fixture.create_ingredient("hair", filling_type)
+
+    boring_recipe = Fixture.create_recipe("boring cookies", [{boring_batter, 1}])
+    nuts_recipe = Fixture.create_recipe("chips cookies", [{batter, 1}, {nuts, 1}])
+    super_chips_recipe = Fixture.create_recipe("super chips cookies", [{batter, 1}, {chips, 2}])
+
+    ingredient_map = [boring_batter, batter, chips, nuts, hair]
+                     |> IngredientMap.from_ingredients
+                     |> IngredientMap.select_ingredient(boring_batter.id)
+                     |> IngredientMap.select_ingredient(batter.id)
+                     |> IngredientMap.select_ingredient(nuts.id)
+                     |> IngredientMap.select_ingredient(batter.id)
+                     |> IngredientMap.select_ingredient(chips.id)
+                     |> IngredientMap.select_ingredient(chips.id)
+                     |> IngredientMap.select_ingredient(hair.id)
+
+    recipes = [boring_recipe, nuts_recipe, super_chips_recipe]
+
     expect_ingredient_map = ingredient_map
+                            |> IngredientMap.deselect_ingredient(boring_batter.id)
                             |> IngredientMap.deselect_ingredient(batter.id)
-                            |> IngredientMap.deselect_ingredient(batter.id)
-                            |> IngredientMap.deselect_ingredient(chips.id)
+                            |> IngredientMap.deselect_ingredient(nuts.id)
                             |> IngredientMap.deselect_ingredient(batter.id)
                             |> IngredientMap.deselect_ingredient(chips.id)
                             |> IngredientMap.deselect_ingredient(chips.id)
@@ -105,7 +137,7 @@ defmodule CookingTest do
     {satisfied_recipes, got_ingredient_map} =
       Cooking.check_recipes(ingredient_map, recipes)
 
-    assert expect_ingredient_map == got_ingredient_map
     assert MapSet.new(satisfied_recipes) == MapSet.new(recipes)
+    assert expect_ingredient_map == got_ingredient_map
   end
 end
